a160bb355f8bfc4809977c01b4f79c2694712140
QUBITABHAY <codingcontestabhay@gmail.com>
2025-02-06 19:58:58 +0530

    add nextAuth

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
+import { error } from "console";
+import { Await } from "react-router-dom";
+
+export async function post(request: NextRequest) {
+
+    try {
+        const {email, password} = await request.json()
+
+        if (!email || !password) {
+            return NextResponse.json(
+                {error: "Email and password are required"},
+                {status: 400}
+            )
+        }
+
+        await connectToDatabase();
+
+        const existingUser = await User.findOne({email})
+
+        if (existingUser) {
+            return NextResponse.json(
+                {error: "Email is  already registered"},
+                {status: 400}
+            )
+        }
+
+        await User.create({
+            email,
+            password,
+        })
+
+        return NextResponse.json(
+            
+            {error: "User registered sucessfully"},
+            {status: 201}
+            
+        )
+
+    } catch (error) {
+        return NextResponse.json(
+            
+            {error: "Failed to register User"},
+            {status: 500}
+            
+        )
+    }
+}
--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { emit } from "process";
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
